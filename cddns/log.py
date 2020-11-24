2cbc572b1472b3b939ff0d4f797ee184c9621617
Chr_ <chr@chrxw.com>
2020-11-24 17:49:54 +0800

    修改日志等级

--- cddns/log.py
+++ cddns/log.py
@@ -2,13 +2,13 @@
 # @Author       : Chr_
 # @Date         : 2020-11-24 12:12:01
 # @LastEditors  : Chr_
-# @LastEditTime : 2020-11-24 13:51:46
+# @LastEditTime : 2020-11-24 17:49:24
 # @Description  : 打印日志
 '''
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(name)s : %(message)s',
                     datefmt='%H:%M:%S')
 
